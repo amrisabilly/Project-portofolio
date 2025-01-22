a6036a9d83414bea1baa37069422d61720c9c0d4
Devsam_ <sabillyamri1@gmail.com>
2025-01-22 23:00:39 +0700

    chek

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
+import { ExperienceMob } from "@/components/Assets";
 // components
 import Navbar from "@/components/Navbar";
 import NavbarMobile from "@/components/NavbarMobile";
@@ -12,12 +13,22 @@ import LogoBunga from "@/public/Images/LogoBunga.svg";
 import LogoBunga1 from "@/public/Images/LogoBunga1.svg";
 import LogoFile from "@/public/Images/File_logo.svg";
 import Background from "@/public/Images/Frame.png";
+import BackgroundMobile from "@/public/mobile.png";
 import Graduation from "@/public/Images/Graduation_logo.svg";
 import Graduation1 from "@/public/Images/Graduation_logo2.svg";
 
 // Profile
 import Photo1 from "@/public/Images/Profile/Photo1.svg";
 import Photo2 from "@/public/Images/Profile/Photo2.svg";
+import Photo3 from "@/public/Images/Profile/Photo3.svg";
+//Grid Mobile
+import Grid1 from "@/public/Images/Grid/grid1.svg";
+import Grid2 from "@/public/Images/Grid/grid2.svg";
+import Grid3 from "@/public/Images/Grid/grid3.svg";
+import Grid4 from "@/public/Images/Grid/grid4.svg";
+import Grid5 from "@/public/Images/Grid/grid5.svg";
+import Grid6 from "@/public/Images/Grid/grid6.svg";
+import Grid7 from "@/public/Images/Grid/grid7.svg";
 
 //Grid Desktop
 import gridDesk1 from "@/public/Images/Grid/Desktop/grid1.svg";
@@ -48,9 +59,9 @@ export default function Home() {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
   return (
-    <div>
+    <div className="bg-primary relative">
       {isMobile ? (
-        <div className="bg-primary w-full h-full">
+        <div className="bg-primary w-full h-full relative">
           <NavbarMobile />
           <div id="home" className="w-full px-8">
             <div className="flex justify-end mt-5 items-center">
@@ -78,7 +89,7 @@ export default function Home() {
                 Bringing 23 years of retail expertise and 13 years of consulting
                 to the table, I specialize in crafting innovative solutions for
                 modern retail and cooperatives, driving growth for MSMEs and
-                communities acrosss Indonesia.
+                communities acros Indonesia.
               </p>
               <a href="/cv.pdf" download>
                 <button
@@ -94,7 +105,324 @@ export default function Home() {
               <Image src={Photo1} alt="Photo" />
             </div>
           </div>
-          
+          <div className="absolute top-[50em]">
+            <div className="relative z-10">
+              <Image
+                src={BackgroundMobile}
+                alt="Background"
+                className="w-full h-auto"
+              />
+              <div className="absolute inset-0 px-8">
+                <Image
+                  src={Photo3}
+                  alt="Photo"
+                  className="mt-[12em] mb-3"
+                  data-aos="fade-up"
+                />
+                <div id="education" className="flex flex-col text-primary">
+                  <div className="flex gap-2 mb-1" data-aos="fade-up">
+                    <Image
+                      src={Graduation}
+                      alt="graduation"
+                      className="w-[20px]"
+                    />
+                    <p className="text-[16px] font-normal">Education</p>
+                  </div>
+                  <h3 className="text-[18px] font-bold mb-2" data-aos="fade-up">
+                    Opened the doors to discovery
+                  </h3>
+                  <p className="text-[12px] font-normal" data-aos="fade-up">
+                    Dedicated to lifelong learning, my education has shaped my
+                    mindset and laid a strong foundation for delivering
+                    impactful solutions in the future.
+                  </p>
+                  <div className="m-4">
+                    <div
+                      className="relative border-l-4 border-[#FFFFFF]"
+                      data-aos="fade-up"
+                    >
+                      {/* Item 1 */}
+                      <div className="mb-2 ml-8">
+                        <div className="absolute w-5 h-5 bg-white rounded-full -left-[12px] border-4 border-[#FF9500]"></div>
+                        <time
+                          className="mb-1 text-[11px] font-normal leading-none text-primary"
+                          data-aos="fade-left"
+                        >
+                          2004
+                        </time>
+                        <h3
+                          className="text-[12px] mt-1 font-bold text-primary"
+                          data-aos="fade-left"
+                        >
+                          UIN Sunan Kalijaga
+                        </h3>
+                        <p
+                          className="text-[12px] font-normal text-primary"
+                          data-aos="fade-left"
+                        >
+                          Bachelor of Islamic Economics
+                        </p>
+                      </div>
+
+                      {/* Item 2 */}
+                      <div className="mb-2 ml-8">
+                        <div className="absolute w-5 h-5 bg-white rounded-full -left-[12px] border-4 border-[#FF9500]"></div>
+                        <time
+                          className="mb-1 text-[11px] font-normal leading-none text-primary"
+                          data-aos="fade-left"
+                        >
+                          2020 - 2023
+                        </time>
+                        <h3
+                          className="text-[12px] mt-1 font-bold text-primary"
+                          data-aos="fade-left"
+                        >
+                          Kelas Prakerja
+                        </h3>
+                        <p
+                          className="text-[12px] font-normal text-primary"
+                          data-aos="fade-left"
+                        >
+                          Manajemen Toko Retail Makanan
+                        </p>
+                      </div>
+
+                      {/* Item 3 */}
+                      <div className="ml-8">
+                        <div className="absolute w-5 h-5 bg-white rounded-full -left-[12px] border-4 border-[#FF9500]"></div>
+                        <time
+                          className="mb-1 text-[11px] font-normal leading-none text-primary"
+                          data-aos="fade-left"
+                        >
+                          2020 - 2023
+                        </time>
+                        <h3
+                          className="text-[12px] mt-1 font-bold text-primary"
+                          data-aos="fade-left"
+                        >
+                          Dinas Koperasi dan UMKM
+                        </h3>
+                        <p
+                          className="text-[12px] font-normal text-primary"
+                          data-aos="fade-left"
+                        >
+                          Pelatihan Manajemen Ritel untuk UMKM
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-full h-[5em] bg-primary shadow-xl rounded-md mt-3 flex items-center gap-3 p-2">
+                  {/* Item 1 */}
+
+                  <div className="flex flex-col items-center">
+                    <div className="flex gap-1 items-end text-secondary">
+                      <h1 className="font-bold text-[20px]">23</h1>
+                      <h3 className="font-bold text-[10px] mb-[5px]">Years</h3>
+                    </div>
+                    <p className="text-center font-normal text-[8px]">
+                      Experienced in Retail Management
+                    </p>
+                  </div>
+
+                  {/* Item 1 */}
+
+                  <div className="flex flex-col items-center">
+                    <div className="flex gap-1 items-end text-secondary">
+                      <h1 className="font-bold text-[20px]">13</h1>
+                      <h3 className="font-bold text-[10px] mb-[5px]">Years</h3>
+                    </div>
+                    <p className="text-center font-normal text-[8px]">
+                      Being a Business Consultant{" "}
+                    </p>
+                  </div>
+
+                  {/* Item 1 */}
+
+                  <div className="flex flex-col items-center">
+                    <div className="flex gap-1 items-end text-secondary">
+                      <h1 className="font-bold text-[20px]">10</h1>
+                      <h3 className="font-bold text-[10px] mb-[5px]">+</h3>
+                    </div>
+                    <p className="text-center font-normal text-[8px]">
+                      Active Community{" "}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-full h-[100em] bg-primary px-6 mb-48">
+            {/* Section 3 */}
+            <div
+              id="experience"
+              className="flex flex-col items-center text-center mt-[43em]"
+            >
+              <div className="flex items-end gap-3 mb-3 " data-aos="fade-up">
+                <Image
+                  src={Graduation1}
+                  alt="graduation"
+                  className="w-[20px]"
+                />
+                <h4 className="text-[16px] font-normal">Experience</h4>
+              </div>
+              <h3
+                className="md:text-[18px] font-bold w-[20em] mb-3"
+                data-aos="fade-up"
+              >
+                Driving Success with Decades of Professional Experience
+              </h3>
+              <p className="text-[12px] font-normal mb-8" data-aos="fade-up">
+                With a passion for retail management and consulting, Ive
+                delivered impactful solutions that drive growth and innovation
+                for businesses.
+              </p>
+              <div className="grid grid-cols-2 gap-4">
+                {ExperienceMob.map((item) => (
+                  <div
+                    className="bg-primary h-[15em] shadow-lg rounded-xl flex flex-col justify-evenly py-4 px-4 text-[12px] text-left"
+                    key={item.id}
+                    data-aos="fade-up"
+                  >
+                    <p className="font-medium text-[#616161] text-[11px]">
+                      {item.tahun}
+                    </p>
+                    <h3 className="font-bold">{item.jabatan}</h3>
+                    <p className="font-normal">{item.keterangan}</p>
+                  </div>
+                ))}
+                <div className="bg-transparent rounded-xl flex flex-col justify-center items-center col-span-2">
+                  <Image
+                    src={LogoBunga1}
+                    alt="logo"
+                    className="w-[155px]"
+                    data-aos="fade-up"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div
+              id="gallery"
+              className="flex flex-col items-center text-center mt-[3em]"
+            >
+              <div className="flex items-end gap-3 mb-3 " data-aos="fade-up">
+                <Image
+                  src={Graduation1}
+                  alt="graduation"
+                  className="w-[20px]"
+                />
+                <h4 className="text-[16px] font-normal">Gallery</h4>
+              </div>
+              <h3
+                className="md:text-[18px] font-bold w-[20em] mb-3"
+                data-aos="fade-up"
+              >
+                Gallery of my Activities{" "}
+              </h3>
+              <div className="">
+                <div className="grid grid-cols-3 gap-2">
+                  <div className="col-span-3">
+                    <Image
+                      src={Grid1}
+                      alt="Group Event"
+                      className=""
+                      data-aos="fade-up"
+                    />
+                  </div>
+                  <div className="col-span-1">
+                    <div className="grid grid-rows-1 gap-2">
+                      {/* Row 1 */}
+                      <div
+                        className="row-span-1 relative h-[54px]"
+                        data-aos="fade-right"
+                      >
+                        <Image
+                          src={Grid2}
+                          alt="Group Event"
+                          className="w-full h-full object-cover rounded-[8.46px]"
+                        />
+                      </div>
+
+                      {/* Row 2 */}
+                      <div
+                        className="row-span-1 relative h-[110px]"
+                        data-aos="fade-right"
+                      >
+                        <Image
+                          src={Grid4}
+                          alt="Group Event"
+                          className="w-full h-full object-cover rounded-[8.46px]"
+                        />
+                      </div>
+
+                      {/* Row 3 */}
+                      <div
+                        className="row-span-1 relative h-[97px]"
+                        data-aos="fade-right"
+                      >
+                        <Image
+                          src={Grid7}
+                          alt="Group Event"
+                          className="w-full h-full object-cover rounded-[8.46px]"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-span-2">
+                    <div className="grid grid-rows-3 gap-2 h-[270px]">
+                      <div
+                        className="row-span-1 relative h-full"
+                        data-aos="fade-left"
+                      >
+                        <Image
+                          src={Grid3}
+                          alt="Group Event"
+                          className="absolute inset-0 w-full h-full object-cover rounded-[8.46px]"
+                        />
+                      </div>
+                      <div className="row-span-2">
+                        <div className="grid grid-cols-2 gap-2 h-[184px]">
+                          {/* Kolom 1 */}
+                          <div
+                            className="col-span-1 relative h-full "
+                            data-aos="fade-up"
+                          >
+                            <Image
+                              src={Grid5}
+                              alt="Group Event"
+                              className="absolute inset-0 w-full h-full object-cover rounded-[8.46px]"
+                            />
+                          </div>
+
+                          {/* Kolom 2 */}
+                          <div
+                            className="col-span-1 relative h-full"
+                            data-aos="fade-left"
+                          >
+                            <Image
+                              src={Grid6}
+                              alt="Group Event"
+                              className="absolute inset-0 w-full h-full object-cover rounded-[8.46px]"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-10" data-aos="fade-up">
+              <Scroll />
+            </div>
+
+            <div
+              data-aos="fade-up"
+              className="w-full h-[0.5em] bg-gradient-to-r from-[#660066] to-[#E1306C] rounded-xl mt-10"
+            ></div>
+          </div>
         </div>
       ) : (
         // Tampilan Desktop
